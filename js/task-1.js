f367c92fe9da9ff5942d84c0ba2ff98bce0625f1
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-03-14 11:58:35 +0200

    js

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,5 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
-
-
-
-function processOrder(quantity, pricePerDroid, customerCredits) {
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
     const totalPrice = quantity * pricePerDroid;
 
     if (totalPrice > customerCredits) {
--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,3 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding) * 2;
-    const borderWidth = parseFloat(border) * 2;
-
-    return contentWidth + paddingWidth + borderWidth;
-}
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-
 
 function checkForSpam(message) {
     const lowerCaseMessage = message.toLowerCase();
